babd7325c6ce072c48b994771b0c2307e98788dc
Edafan4uk <edikzaxarin@gmail.com>
2025-03-31 20:58:57 +0300

    Added dockerfile; disabled https redirects

--- src/BsdOrderBook.Host/Program.cs
+++ src/BsdOrderBook.Host/Program.cs
@@ -22,13 +22,13 @@ builder.Services.AddValidatorsFromAssemblyContaining<OrderInputValidator>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
